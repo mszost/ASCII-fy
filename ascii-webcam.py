88a33eddd5640efe0a4c8b7b6619343c8f2afbb2
Michael Szostak <mzostak@gmail.com>
2022-12-06 11:41:48 -0600

    fix absolute path bug

--- ascii-webcam.py
+++ ascii-webcam.py
@@ -1,21 +1,21 @@
 import os, sys, cv2, curses
 from textwrap import dedent
 
-x = 640
-y = 480
+# x = 640
+# y = 480
+mode, img_path, vid_path = None, None, None
+
 
 def convert_img(img_path):
-
-    #############
-    img_path = 'C:\\Users\\msz\\Lewis\\cpsc\\sprint-08\\assets\\cat.jpg'
-    #############
+    # img_path = 'C:\\Users\\msz\\Lewis\\cpsc\\sprint-08\\ASCII-Text-Converter\\assets\\cat.jpg'
+    # C:\Users\msz\Lewis\cpsc\sprint-08\ASCII-Text-Converter\assets\cat.jpg
 
     if ':' in img_path: # path is absolute
        filename = img_path
 
     else: # path is relative
-        dirname = os.path.dirname(__file__) # gets parent directory that this file is stored in
-        filename = os.path.join(dirname, img_path) # uses parent dir to get absolute path of img
+        dirname = os.path.dirname(__file__) # gets absolute path of this file
+        filename = os.path.join(dirname, img_path) # merges directory tree of this file to find absolute path of the given relative img_path
 
     img_ascii = cv2.imread(filename)
     cv2.imshow('Image', img_ascii)
@@ -24,45 +24,65 @@ def convert_img(img_path):
     cv2.destroyAllWindows()
 
 
+def convert_vid(vid_path):
+    print('\nLoading video ...')
+    # TODO: Implement video conversion
+
+
 def show_webcam(mirror=True):
-    print('\nLoading Webcam ...')
+    print('\nLoading camera ...')
     cap = cv2.VideoCapture(0)
-    print('Press ESC to exit.')
+    print('Press ESC on focused window to close')
 
     while True:
-        ret, vid = cap.read()
+        ret, frame = cap.read()
 
-        if mirror: 
-            vid = cv2.flip(vid, 1)
+        if mirror: frame = cv2.flip(frame, 1)
 
-        cv2.imshow('Webcam view', vid)
+        cv2.imshow('Camera view', frame)
 
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
+        if cv2.waitKey(1) == 27: break  # esc to quit
     cv2.destroyAllWindows()
+
+
 
 while True:
     mode = input(dedent('''
-    1 = convert_img()
-    2 = TODO
-    3 = show_webcam()
+    #################
+    #               #
+    #  0 = QUIT     #
+    #  1 = Image    #
+    #  2 = Video    #
+    #  3 = Webcam   #
+    #               #
+    #################
     '''))
 
-    if mode == '1':
-        img_path = input('Enter the path to the desired image: ') # At least 2 of the images I tested didn't work, but all of the others did. I couldn't figure out what was causing issues wiht the ones that didn't, they weren't special in any way compared to the others
+    if mode == '0': break
 
-        try:
-            convert_img(img_path)
-            break
-        except:
-            print('\nError convering image! Double check entered file path.')
+    if mode == '1':
+        while True:
+            img_path = input('\nEnter the path to the desired image: ')# At least 2 of the images I tested didn't work, but all of the others did. I couldn't figure out what was causing issues with the ones that didn't, as they had no noticeable differences from the ones that did work.
+            if img_path == '0': break
+
+            try:
+                convert_img(img_path)
+                break
+            except:
+                print('\nError converting image! Double check file path, or enter "0" to go back.')
 
     elif mode == '2':
-        break
-        # TODO: Implement video file option
+        while True:
+            vid_path = input('\nEnter the path to the desired video: ')
+            if vid_path == '0': break
+
+            try:
+                convert_vid(vid_path)
+                break
+            except:
+                print('\nError converting video! Double check file path, or enter "0" to go back.')
 
     elif mode == '3':
         show_webcam()
-        break
 
     else: print('INVALID INPUT')
